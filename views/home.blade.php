a0061a522ab6dbb12030922d731e550c0ce7f278
sagsoz06 <sagsoz06@gmail.com>
2017-12-17 14:40:33 +0300

    Updating theme

--- views/home.blade.php
+++ views/home.blade.php
@@ -4,20 +4,20 @@
     @include('partials.home.sliders.1')
 
 	@if(isset($page->settings->show_box))
-	@if($page->settings->show_box)
-	<section class="page-section" style="padding:30px 0 0 0; text-align:justify;">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-				@if(isset($page->settings->sub_title->{locale()}))
-				<h1 class="text-center">{{ $page->settings->sub_title->{locale()} }}</h1>
-				@endif
-				{!! $page->body !!}
+		@if($page->settings->show_box)
+		<section class="page-section" style="padding:30px 0 0 0; text-align:justify;">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+					@if(isset($page->settings->sub_title->{locale()}))
+					<h1 class="text-center">{{ $page->settings->sub_title->{locale()} }}</h1>
+					@endif
+					{!! $page->body !!}
+					</div>
 				</div>
 			</div>
-		</div>
-	</section>
-	@endif
+		</section>
+		@endif
 	@endif
 
     @include('partials.cars.slider')
